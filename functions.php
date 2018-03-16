fd0d191817f400f146c5d8a8b0d089cf4c705814
Bryan Cady <sw@listentothewindmedia.com>
2018-03-16 14:50:09 -0400

    Removed entry meta

--- functions.php
+++ functions.php
@@ -288,3 +288,5 @@ function howdy_message($translated_text, $text, $domain) {
 }
 add_filter('gettext', 'howdy_message', 10, 3);
 
+//* Remove the entry meta in the entry header (requires HTML5 theme support)
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
